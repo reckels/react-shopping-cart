e7f8aa8f4fef0b363b724a1241b6c0c524f94b9f
Rachel Eckels <Rachel.Eckels@walmart.com>
2022-02-07 13:16:31 -0600

    item return fixed

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -7,20 +7,22 @@ import {items} from './static-data';
 
 const App = () => {
   const [activeTab, setActiveTab] = useState('items');
+  const [cart, setCart] = useState([]);
+  const addToCart = (item) => {setCart(cart => [...cart, item]);};
   return (  
     <div className="App">
       <Nav activeTab={activeTab} onTabChange={setActiveTab}/>
       <main className="App-content">
-        <Content tab={activeTab}/>
+        <Content tab={activeTab} onAddToCart={addToCart}/>
       </main>
     </div>
   )
 }
 
-const Content = ({tab}) => {
+const Content = ({tab, onAddToCart}) => {
   switch(tab){
     case 'items':
-      return <ItemPage items={items}/>;
+      return <ItemPage items={items} onAddToCart={onAddToCart}/>;
     case 'cart':
       return <span>the cart</span>;
     default: break;
